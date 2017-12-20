4ae4cd990d696368f77ed180c1e8532392260cde
Shehan Perera <12shehanakalanka@gmail.com>
2017-12-20 10:35:35 +0530

    Update SampleClass.java

--- SampleClass.java
+++ SampleClass.java
@@ -5,8 +5,15 @@ public class SampleClass {
     /*
     *This is the main class this will execute as normal way.
     *premain will run before this.
-
+             If you want to run this in IDE without using console and jar uncomment [1]
      */
+    /* [1]
+   static Instrumentation ins;
+    static {
+        Agent.premain("",ins);
+    }
+    */
+    
     public static void main(String[] args) {
 
         System.out.println("This is Main Method");
